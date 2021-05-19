da913d6384864393ad690b360ff94f05417dc24d
galba-rp <yaroslav.hontar@gmail.com>
2021-05-19 09:35:25 +0200

    doc: add necessary comments to replies_controller

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,26 +1,22 @@
 class RepliesController < ApplicationController
   REPLIES_PER_PAGE = 25
   
-
   before_action :require_logged_in_user, :flag_warning, :set_page
   after_action :update_read_ribbons, only: [:unread]
 
   def all
-    # render action: 'placeholder' and return
     @heading = @title = "All Your Replies"
     @replies = Comment.where(user_id: @user.id)
                  .for_user(@user.id)
                  .offset((@page - 1) * REPLIES_PER_PAGE)
                  .limit(REPLIES_PER_PAGE)
-    # apply_current_vote // not implemented yet
+    # apply_current_vote // depends on "current_vote_vote" attribute which does not exist in this version
     render :show
     
   end
 
   def comments
-    # render action: 'placeholder' and return
     @heading = @title = "Your Comment Replies"   
-    # @replies = Comment.where("user_id = @user.id and parent_comment_id != 'nil'")
     @replies = Comment.comment_replies_for(@user.id)
                  .offset((@page - 1) * REPLIES_PER_PAGE)
                  .limit(REPLIES_PER_PAGE)
@@ -29,7 +25,6 @@ class RepliesController < ApplicationController
   end
 
   def stories
-    # render action: 'placeholder' and return
     @heading = @title = "Your Story Replies"
     @replies = Comment
                 .story_replies_for(@user.id)
@@ -40,7 +35,6 @@ class RepliesController < ApplicationController
   end
 
   def unread
-    # render action: 'placeholder' and return
     @heading = @title = "Your Unread Replies"
     @replies = ReplyingComment.unread_replies_for(@user.id)
     # apply_current_vote
