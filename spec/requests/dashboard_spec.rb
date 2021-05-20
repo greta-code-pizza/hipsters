0ff653938c668b559176127ee159f46b81101ef7
corvaisier <julien.corvaisier.br@gmail.com>
2021-05-20 15:08:59 +0200

    add coverage test to dashboard

---
+++ spec/requests/dashboard_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "Reading Dashboard", type: :feature do
+
+  feature "when logged in" do
+    let(:admin) { create(:user) }
+    before(:each) { stub_login_as admin }
+
+    scenario "go to dashboard" do
+      visit "/dashboard"
+      expect(page).to have_content(User.all.count)
+      expect(page).to have_content(Story.all.count)
+    end
+  end
+end
