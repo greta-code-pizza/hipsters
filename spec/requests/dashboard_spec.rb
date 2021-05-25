cf45137a6600c7e9731a26eca7db5de00aaffb93
corvaisier <julien.corvaisier.br@gmail.com>
2021-05-25 11:21:13 +0200

    correct tests

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -3,13 +3,29 @@ require 'rails_helper'
 RSpec.feature "Reading Dashboard", type: :feature do
 
   feature "when logged in" do
+    let(:user) { create(:user) }
     let(:admin) { create(:user, :admin) }
-    before(:each) { stub_login_as admin }
 
-    scenario "go to dashboard" do
+    scenario "go to dashboard with admin user" do
+      stub_login_as admin
+      
       visit "/dashboard"
       expect(page).to have_content(User.all.count)
       expect(page).to have_content(Story.all.count)
     end
+
+    scenario "go to login with standard user" do
+      stub_login_as user
+      
+      visit "/dashboard"
+      expect(page).to have_css(".flash-error")
+    end
+
+    scenario "go to login with unlogged user" do
+      visit "/dashboard"
+      expect(page).to have_selector("form")
+      expect(page).to have_field("email")
+    end
   end
 end
+
